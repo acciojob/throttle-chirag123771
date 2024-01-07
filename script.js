81f1c6f5e2521bc46e63fd5695556e17bf7eeedf
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-01-08 02:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 //your JS code here. If required.
 function throttle(callback, delay) {
+let timeoutId;
+  let lastArgs;
+  let lastThis;
 
+  function throttled(...args) {
+    const now = Date.now();
+    lastArgs = args;
+    lastThis = this;
+
+    if (!timeoutId) {
+      callback.apply(lastThis, lastArgs);
+      timeoutId = setTimeout(() => {
+        timeoutId = null;
+        if (lastArgs) {
+          callback.apply(lastThis, lastArgs);
+          lastArgs = null;
+          lastThis = null;
+        }
+      }, delay);
+    }
+  }
+
+  throttled.cancel = function () {
+    clearTimeout(timeoutId);
+    timeoutId = null;
+    lastArgs = null;
+    lastThis = null;
+  };
+
+  return throttled;
 }
 
 module.exports = throttle;
